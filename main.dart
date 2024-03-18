b14748f0f3b1070ddf5dd46e2c811263ad731ccf
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2024-03-17 22:24:21 -0300

    feature: Implementação de AST

--- main.dart
+++ main.dart
@@ -76,6 +76,81 @@ class Tokenizer {
   }
 }
 
+// class Parser {
+//   late final Tokenizer tokenizer;
+
+//   Parser(String source) {
+//     tokenizer = Tokenizer(source);
+//   }
+
+//   int parseExpression() {
+//     int result = parseTerm();
+//     while (tokenizer.next.type == TokenType.plus ||
+//         tokenizer.next.type == TokenType.minus) {
+//       var operator = tokenizer.next.type;
+//       tokenizer.selectNext(); // Consume operator
+//       int right = parseTerm();
+//       if (operator == TokenType.plus) {
+//         result += right;
+//       } else {
+//         result -= right;
+//       }
+//     }
+//     return result;
+//   }
+
+//   int parseTerm() {
+//     int result = parseFactor();
+//     while (tokenizer.next.type == TokenType.multiply ||
+//         tokenizer.next.type == TokenType.divide) {
+//       var operator = tokenizer.next.type;
+//       tokenizer.selectNext(); // Consume operator
+//       int right = parseFactor();
+//       if (operator == TokenType.multiply) {
+//         result *= right;
+//       } else {
+//         result ~/= right; // Use integer division
+//       }
+//     }
+//     return result;
+//   }
+
+//   int parseFactor() {
+//     if (tokenizer.next.type == TokenType.integer) {
+//       int value = tokenizer.next.value;
+//       tokenizer.selectNext(); // Consume number
+//       return value;
+//     } else if (tokenizer.next.type == TokenType.minus) {
+//       tokenizer.selectNext(); // Consume operator
+//       return -parseFactor();
+//     } else if (tokenizer.next.type == TokenType.plus) {
+//       tokenizer.selectNext(); // Consume operator
+//       return parseFactor();
+//     } else if (tokenizer.next.type == TokenType.openParen) {
+//       tokenizer.selectNext(); // Consume '('
+//       int result = parseExpression();
+//       if (tokenizer.next.type != TokenType.closeParen) {
+//         throw FormatException("Expected ')' but found ${tokenizer.next.type}");
+//       }
+//       tokenizer.selectNext(); // Consume ')'
+//       return result;
+//     } else {
+//       throw FormatException("Expected number but found ${tokenizer.next.type}");
+//     }
+//   }
+
+//   int run() {
+//     int result = parseExpression();
+//     if (tokenizer.next.type != TokenType.eof) {
+//       throw FormatException(
+//           'Unexpected token ${tokenizer.next.type} at the end of the expression');
+//     }
+//     return result;
+//   }
+// }
+
+// Now the class Parser must build the AST instead of evaluating the expression
+
 class Parser {
   late final Tokenizer tokenizer;
 
@@ -83,52 +158,44 @@ class Parser {
     tokenizer = Tokenizer(source);
   }
 
-  int parseExpression() {
-    int result = parseTerm();
+  Node parseExpression() {
+    Node result = parseTerm();
     while (tokenizer.next.type == TokenType.plus ||
         tokenizer.next.type == TokenType.minus) {
       var operator = tokenizer.next.type;
       tokenizer.selectNext(); // Consume operator
-      int right = parseTerm();
-      if (operator == TokenType.plus) {
-        result += right;
-      } else {
-        result -= right;
-      }
+      Node right = parseTerm();
+      result = BinOp(result, right, operator.toString());
     }
     return result;
   }
 
-  int parseTerm() {
-    int result = parseFactor();
+  Node parseTerm() {
+    Node result = parseFactor();
     while (tokenizer.next.type == TokenType.multiply ||
         tokenizer.next.type == TokenType.divide) {
       var operator = tokenizer.next.type;
       tokenizer.selectNext(); // Consume operator
-      int right = parseFactor();
-      if (operator == TokenType.multiply) {
-        result *= right;
-      } else {
-        result ~/= right; // Use integer division
-      }
+      Node right = parseFactor();
+      result = BinOp(result, right, operator.toString());
     }
     return result;
   }
 
-  int parseFactor() {
+  Node parseFactor() {
     if (tokenizer.next.type == TokenType.integer) {
       int value = tokenizer.next.value;
       tokenizer.selectNext(); // Consume number
-      return value;
+      return IntVal(value);
     } else if (tokenizer.next.type == TokenType.minus) {
       tokenizer.selectNext(); // Consume operator
-      return -parseFactor();
+      return UnOp(parseFactor(), '-');
     } else if (tokenizer.next.type == TokenType.plus) {
       tokenizer.selectNext(); // Consume operator
-      return parseFactor();
+      return UnOp(parseFactor(), '+');
     } else if (tokenizer.next.type == TokenType.openParen) {
       tokenizer.selectNext(); // Consume '('
-      int result = parseExpression();
+      Node result = parseExpression();
       if (tokenizer.next.type != TokenType.closeParen) {
         throw FormatException("Expected ')' but found ${tokenizer.next.type}");
       }
@@ -139,8 +206,8 @@ class Parser {
     }
   }
 
-  int run() {
-    int result = parseExpression();
+  Node run() {
+    Node result = parseExpression();
     if (tokenizer.next.type != TokenType.eof) {
       throw FormatException(
           'Unexpected token ${tokenizer.next.type} at the end of the expression');
@@ -149,13 +216,89 @@ class Parser {
   }
 }
 
+class PrePro {
+  String filter(String source) {
+    return source.replaceAll(RegExp(r'--.*'), '');
+  }
+}
+
+abstract class Node {
+  dynamic value;
+  List<Node> children = [];
+  Node(this.value);
+
+  dynamic Evaluate() {
+    for (var child in children) {
+      child.Evaluate();
+    }
+    ;
+  }
+}
+
+class BinOp extends Node {
+  final Node left;
+  final Node right;
+  BinOp(this.left, this.right, String op) : super(op);
+
+  @override
+  dynamic Evaluate() {
+    switch (value) {
+      case "TokenType.plus":
+        return left.Evaluate() + right.Evaluate();
+      case "TokenType.minus":
+        return left.Evaluate() - right.Evaluate();
+      case "TokenType.multiply":
+        return left.Evaluate() * right.Evaluate();
+      case "TokenType.divide":
+        return left.Evaluate() ~/ right.Evaluate();
+      default:
+        throw Exception('Invalid operator: $value');
+    }
+  }
+}
+
+class UnOp extends Node {
+  final Node expr;
+  UnOp(this.expr, String op) : super(op);
+
+  @override
+  dynamic Evaluate() {
+    if (value == '-') {
+      return -expr.Evaluate();
+    } else {
+      return expr.Evaluate();
+    }
+  }
+}
+
+class IntVal extends Node {
+  IntVal(int value) : super(value);
+
+  @override
+  dynamic Evaluate() {
+    return value;
+  }
+}
+
+class NoOp extends Node {
+  NoOp() : super(null);
+
+  @override
+  dynamic Evaluate() {
+    return null;
+  }
+}
+
 void main(List<String> args) {
   if (args.isEmpty) {
     throw ArgumentError('Please provide an expression to parse');
   }
+  PrePro prePro = PrePro();
+  String filtered = prePro.filter(args[0]);
   try {
-    final parser = Parser(args[0]);
-    final result = parser.run();
+    final parser = Parser(filtered);
+    final ast = parser.run();
+    final result = ast.Evaluate();
     stdout.writeln(result);
   } catch (e) {
     throw Exception(e);
