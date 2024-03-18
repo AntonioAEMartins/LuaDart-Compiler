e3e33b61b548463f194296d313bc072eb58f635b
Antônio A E Martins <81169982+AntonioAEMartins@users.noreply.github.com>
2024-03-17 23:07:19 -0300

    fix: cleaning code

--- main.dart
+++ main.dart
@@ -76,81 +76,6 @@ class Tokenizer {
   }
 }
 
-// class Parser {
-//   late final Tokenizer tokenizer;
-
-//   Parser(String source) {
-//     tokenizer = Tokenizer(source);
-//   }
-
-//   int parseExpression() {
-//     int result = parseTerm();
-//     while (tokenizer.next.type == TokenType.plus ||
-//         tokenizer.next.type == TokenType.minus) {
-//       var operator = tokenizer.next.type;
-//       tokenizer.selectNext(); // Consume operator
-//       int right = parseTerm();
-//       if (operator == TokenType.plus) {
-//         result += right;
-//       } else {
-//         result -= right;
-//       }
-//     }
-//     return result;
-//   }
-
-//   int parseTerm() {
-//     int result = parseFactor();
-//     while (tokenizer.next.type == TokenType.multiply ||
-//         tokenizer.next.type == TokenType.divide) {
-//       var operator = tokenizer.next.type;
-//       tokenizer.selectNext(); // Consume operator
-//       int right = parseFactor();
-//       if (operator == TokenType.multiply) {
-//         result *= right;
-//       } else {
-//         result ~/= right; // Use integer division
-//       }
-//     }
-//     return result;
-//   }
-
-//   int parseFactor() {
-//     if (tokenizer.next.type == TokenType.integer) {
-//       int value = tokenizer.next.value;
-//       tokenizer.selectNext(); // Consume number
-//       return value;
-//     } else if (tokenizer.next.type == TokenType.minus) {
-//       tokenizer.selectNext(); // Consume operator
-//       return -parseFactor();
-//     } else if (tokenizer.next.type == TokenType.plus) {
-//       tokenizer.selectNext(); // Consume operator
-//       return parseFactor();
-//     } else if (tokenizer.next.type == TokenType.openParen) {
-//       tokenizer.selectNext(); // Consume '('
-//       int result = parseExpression();
-//       if (tokenizer.next.type != TokenType.closeParen) {
-//         throw FormatException("Expected ')' but found ${tokenizer.next.type}");
-//       }
-//       tokenizer.selectNext(); // Consume ')'
-//       return result;
-//     } else {
-//       throw FormatException("Expected number but found ${tokenizer.next.type}");
-//     }
-//   }
-
-//   int run() {
-//     int result = parseExpression();
-//     if (tokenizer.next.type != TokenType.eof) {
-//       throw FormatException(
-//           'Unexpected token ${tokenizer.next.type} at the end of the expression');
-//     }
-//     return result;
-//   }
-// }
-
-// Now the class Parser must build the AST instead of evaluating the expression
-
 class Parser {
   late final Tokenizer tokenizer;
 
@@ -217,15 +142,12 @@ class Parser {
 }
 
 String cleanInput(String input) {
-  // Regex que identifica caracteres inválidos
   RegExp invalidCharacters = RegExp(r'[^0-9+\-*/\(\)\s]');
-  // Remove caracteres inválidos da entrada
   return input.replaceAll(invalidCharacters, '');
 }
 
 class PrePro {
   String filter(String source) {
-    // Remove comentários iniciados por --
     String noComments = source.replaceAll(RegExp(r'--.*'), '');
     String cleaned = cleanInput(noComments);
     return cleaned;
